f6f4079d84e6ac65a191e5ba4c66cfab156e860c
iahmedelbayaa <iahmedelbayaa>
2025-09-27 15:11:46 +0300

    feat: update AppModule to include UserController, UserService, and Neo4jModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { Neo4jModule } from './neo4j/neo4j.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [Neo4jModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
